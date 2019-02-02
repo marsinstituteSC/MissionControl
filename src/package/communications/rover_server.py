ec40aa8bb483e7d523639f42416c2422b234038a
Bernt Andreas Eide <bernta1@msn.com>
2019-02-02 18:26:50 +0100

    - Extended the communications to now use a proper multicast publisher for the sensor data from the Rover side, the Rover will simply stream changes to a multicast address on the local net, any device on the net can then pickup these messages.
    - Refactored code.

--- src/package/communications/rover_server.py
+++ src/package/communications/rover_server.py
@@ -1,9 +1,22 @@
 """ Starts a test server which can act as the Rover UDP server. """
 
-import PyQt5.QtCore
-import PyQt5.QtNetwork
+import time
+import json
+import random
+import signal
+import sys
 
-import time, json, random
+from PyQt5.QtNetwork import QUdpSocket, QHostAddress, QNetworkInterface
+
+GAMEPAD_SERVER_ADDRESS = QHostAddress("127.0.0.1")
+GAMEPAD_SERVER_PORT = 5000
+
+SENSOR_PUBLISH_SERVER = QHostAddress("239.255.43.21")
+SENSOR_PUBLISH_PORT = 45454
+
+RUNNING = True
+
+
 def generate_random_data():
     """
     Generates random sensor data and returns a json object
@@ -19,20 +32,52 @@ def generate_random_data():
     # Temperature generation
     output["temperature"] = random.randint(-60, 21)
 
-    return json.dumps(output)
+    return json.dumps(output).encode()
+
+
+def startGamepadListener():
+    s = QUdpSocket()
+    s.bind(GAMEPAD_SERVER_ADDRESS, GAMEPAD_SERVER_PORT)
+    return s
+
+
+def startSensorMulticastPublisher():
+    s = QUdpSocket()
+    s.bind(QHostAddress.AnyIPv4, 0)
+    return s
+
+
+def shutdown(sig, frame):
+    global RUNNING
+    RUNNING = False
 
 
 if __name__ == "__main__":
-    s = PyQt5.QtNetwork.QUdpSocket()
-    addr = PyQt5.QtNetwork.QHostAddress("127.0.0.1")
-    port = 5000
-    s.bind(addr, port)
+    # Receive gamepad changes from client (control station)
+    gamepadServer = startGamepadListener()
+    # Start a multicast publisher which writes to the given mcast addr. so that we can read a 'stream' of sensor changes, multiple clients can read from this stream too!
+    sensorPublisher = startSensorMulticastPublisher()
+
+    signal.signal(signal.SIGINT, shutdown)
+
     b = "Yes, I'm here!".encode()
-    while True:
+    while RUNNING:
         print("waiting...")
-        if s.hasPendingDatagrams():
-            data, inAddress, inPort = s.readDatagram(s.pendingDatagramSize())
+
+        if gamepadServer.hasPendingDatagrams():
+            data, inAddress, inPort = gamepadServer.readDatagram(
+                gamepadServer.pendingDatagramSize())
             print("Recv:", data.decode())
-            s.writeDatagram(b, inAddress, inPort)
+            gamepadServer.writeDatagram(b, inAddress, inPort)
+
+        print("Sending on multicast!")
+        sensorPublisher.writeDatagram(
+            generate_random_data(), SENSOR_PUBLISH_SERVER, SENSOR_PUBLISH_PORT)
+
         time.sleep(200 / 1000)
 
+    # Cleanup
+    gamepadServer.close()
+    sensorPublisher.close()
+
+    print("Closed server...")
--- src/package/communications/udp_conn.py
+++ src/package/communications/udp_conn.py
@@ -1,33 +1,43 @@
 """ Establishes a UDP connection with the Rover, implements methods for sending & receiving data! """
 
-import PyQt5.QtCore
-import PyQt5.QtNetwork
 import time
 import queue
+
+from PyQt5.QtCore import QThread
+from PyQt5.QtNetwork import QUdpSocket, QHostAddress, QNetworkInterface
 
 from utils import event
 from settings import settings as cfg
 
 # 192.168.1.3 <- actual server addr. to use.
 
-CLIENTHOST = PyQt5.QtNetwork.QHostAddress("127.0.0.1")
+CLIENTHOST = QHostAddress("127.0.0.1")
 CLIENTPORT = 37500
+
+SENSOR_PUBLISH_SERVER = QHostAddress("239.255.43.21")
+SENSOR_PUBLISH_PORT = 45454
 
 ROVERSERVER = None # Allow other files/pgks to easily access our udp server through this global.
 TICK = (50 / 1000) # How often in msec should we check inc / send outgoing msgs.
 
-class UDPRoverServer(PyQt5.QtCore.QThread):
+class UDPRoverServer(QThread):
     def __init__(self):
         super().__init__()
         cfg.SETTINGSEVENT.addListener(self, self.onSettingsChanged)
         self.shouldDestroy = False
         self.loadSettings(cfg.SETTINGS)
         self.messagesToSend = queue.Queue()        
-        self.connect()
+        self.connectToGamepadServer()
+        self.connectToSensorPublisher()
 
-    def connect(self):
-        self.socket = PyQt5.QtNetwork.QUdpSocket(self)
-        self.socket.bind(CLIENTHOST, CLIENTPORT)
+    def connectToGamepadServer(self):
+        self.gamepadSocket = QUdpSocket(self)
+        self.gamepadSocket.bind(CLIENTHOST, CLIENTPORT)
+
+    def connectToSensorPublisher(self):
+        self.sensorpubSocket = QUdpSocket(self)
+        self.sensorpubSocket.bind(QHostAddress.AnyIPv4, SENSOR_PUBLISH_PORT, QUdpSocket.ShareAddress)
+        self.sensorpubSocket.joinMulticastGroup(SENSOR_PUBLISH_SERVER)
 
     def writeToRover(self, data):
         self.messagesToSend.put_nowait(data)
@@ -42,28 +52,32 @@ class UDPRoverServer(PyQt5.QtCore.QThread):
         self.loadSettings(params)
 
     def loadSettings(self, config):
-        # IF client socket must be changed, disconnect old socket first, however changing the client socket is unnecessary as it uses the localhost address anyways.
-        #if self.socket.connected(): # Disconnect socket, unbind properly first.
-        #self.socket.disconnectFromHost()
         self.serverAddress = config.get("main", "serveraddress")
         self.serverPort = int(config.get("main", "serverport"))        
 
     def destroy(self):
         self.shouldDestroy = True
-        self.socket.disconnectFromHost()
+        self.gamepadSocket.close()
+        self.sensorpubSocket.leaveMulticastGroup(SENSOR_PUBLISH_SERVER)
+        self.sensorpubSocket.close()
 
     def run(self):
         while self.shouldDestroy == False:
-            if self.socket.hasPendingDatagrams():
-                data, _, _ = self.socket.readDatagram(self.socket.pendingDatagramSize())
+            if self.gamepadSocket.hasPendingDatagrams():
+                data, _, _ = self.gamepadSocket.readDatagram(self.gamepadSocket.pendingDatagramSize())
                 if data is not None: # TODO Process incoming messages
                     print(data.decode())                    
+
+            if self.sensorpubSocket.hasPendingDatagrams():
+                data, _, _ = self.sensorpubSocket.readDatagram(self.sensorpubSocket.pendingDatagramSize())
+                if data is not None:
+                    print("Received Sensor Data:", data.decode())
 
             # Fetch messages from a thread-safe queue, if empty, skip and wait
             # for TICK time.
             d = self.fetchMessageToSend()
             if d:
-                self.socket.writeDatagram(d.encode(), self.serverAddress, self.serverPort)    
+                self.gamepadSocket.writeDatagram(d.encode(), self.serverAddress, self.serverPort)    
 
             time.sleep(TICK)        
 
