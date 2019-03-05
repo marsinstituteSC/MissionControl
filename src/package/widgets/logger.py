23c2915701294d2aeb612352b2e7f1843c618d8f
Bernt Andreas Eide <bernta1@msn.com>
2019-03-05 22:03:25 +0100

    - Fixed a bug related to dark mode / light mode switching which caused color issues for common items.
    - Removed combo box for priority selection, using check boxes instead, for the logger. Allows better filtering.

--- src/package/widgets/logger.py
+++ src/package/widgets/logger.py
@@ -9,7 +9,6 @@ from PyQt5.QtCore import Qt
 from PyQt5.uic import loadUi
 
 from utils.math import clamp
-from settings.settings import SETTINGSEVENT, SETTINGS
 
 import PyQt5.QtGui
 import datetime
@@ -28,17 +27,17 @@ class LogItem():
 
     def getTableItem(self, v, p):
         item = QTableWidgetItem(v)
-        item.setForeground(self.color)
+        if p != 0: # Common priority uses default label color.
+            item.setForeground(self.color)
         item.setData(Qt.UserRole, p)
         return item
 
 class ColorizedLogger(QWidget):
 
-    def __init__(self, parent=None, colorCommon=QColor(0, 0, 0), colorNotification=QColor(0, 255, 0), colorWarning=QColor(253, 106, 2), colorError=QColor(255, 0, 0)):
+    def __init__(self, parent=None, colorNotification=QColor(0, 255, 0), colorWarning=QColor(253, 106, 2), colorError=QColor(255, 0, 0)):
         super().__init__()
-        SETTINGSEVENT.addListener(self, self.onSettingsChanged)
         self.setParent(parent)
-        self.colorForPriority = [colorCommon if (SETTINGS.get("main", "stylesheet") == "False") else QColor(255, 255, 255) , colorNotification, colorWarning, colorError]
+        self.colorForPriority = [None, colorNotification, colorWarning, colorError]
         self.data = {
             "0": [],
             "1": [],
@@ -46,19 +45,6 @@ class ColorizedLogger(QWidget):
             "3": []
         }
         self.setupUi()
-
-    def __del__(self):
-        if SETTINGSEVENT:
-            SETTINGSEVENT.removeListener(self)
-
-    def onSettingsChanged(self, name, params):
-        self.colorForPriority[0] = QColor(0, 0, 0) if (params.get("main", "stylesheet") == "False") else QColor(255, 255, 255)
-        for r in range(0, self.loggerTable.rowCount()): # Update the low prio. elements!
-            priority = self.loggerTable.item(r, 0).data(Qt.UserRole)
-            if priority == 0:
-                self.loggerTable.item(r, 0).setForeground(self.colorForPriority[0])
-                self.loggerTable.item(r, 1).setForeground(self.colorForPriority[0])
-                self.loggerTable.item(r, 2).setForeground(self.colorForPriority[0])
 
     def keyPressEvent(self, e):
         super().keyPressEvent(e)
@@ -70,11 +56,23 @@ class ColorizedLogger(QWidget):
         header = self.loggerTable.horizontalHeader()
         header.setSectionResizeMode(0, QHeaderView.Stretch)
         header.setSectionResizeMode(1, QHeaderView.ResizeToContents)
-        header.setSectionResizeMode(2, QHeaderView.ResizeToContents)        
+        header.setSectionResizeMode(2, QHeaderView.ResizeToContents)       
+
         self.searchButton.clicked.connect(self.display)
         self.checkFilterGUI.stateChanged.connect(self.display)
         self.checkFilterRover.stateChanged.connect(self.display)
-        self.comboPriorities.currentIndexChanged.connect(self.display)
+        self.checkPrioCommon.stateChanged.connect(self.display)
+        self.checkPrioNotif.stateChanged.connect(self.display)
+        self.checkPrioWarn.stateChanged.connect(self.display)
+        self.checkPrioErr.stateChanged.connect(self.display)
+
+        self.checkPrio = {
+            0: self.checkPrioCommon,
+            1: self.checkPrioNotif,
+            2: self.checkPrioWarn,
+            3: self.checkPrioErr
+        }
+
         self.show()
 
     def getColorForPriority(self, priority):
@@ -98,7 +96,6 @@ class ColorizedLogger(QWidget):
 
     def logData(self, text, priority, type=0):
         """Log the data in a dictionary"""
-        prioChoice = (self.comboPriorities.currentIndex() - 1)
         priority = clamp(priority, 0, (len(self.colorForPriority)-1))
         color = self.getColorForPriority(priority)
         tim = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
@@ -106,7 +103,7 @@ class ColorizedLogger(QWidget):
         self.data.get(str(priority)).append(item)     
 
         # Check if we should add this data to the table right away:
-        if (not self.checkFilterGUI.isChecked() and type is 0) or (not self.checkFilterRover.isChecked() and type is 1) or ((prioChoice >= 0) and prioChoice is not item.priority) or (not self.matchesCriteria(item, self.searchText.text())):
+        if (not self.checkFilterGUI.isChecked() and type is 0) or (not self.checkFilterRover.isChecked() and type is 1) or (not self.checkPrio[priority].isChecked()) or (not self.matchesCriteria(item, self.searchText.text())):
             return
 
         self.addNewRow(item)  
@@ -122,13 +119,12 @@ class ColorizedLogger(QWidget):
 
     def display(self):
         search = self.searchText.text()
-        prioChoice = (self.comboPriorities.currentIndex() - 1)
         self.loggerTable.clearContents()
         self.loggerTable.setRowCount(0)
 
         for p, _ in self.data.items():
             for v in self.data[p]:
-                if (not self.checkFilterGUI.isChecked() and v.type is 0) or (not self.checkFilterRover.isChecked() and v.type is 1) or ((prioChoice >= 0) and prioChoice is not v.priority) or (not self.matchesCriteria(v, search)):
+                if (not self.checkFilterGUI.isChecked() and v.type is 0) or (not self.checkFilterRover.isChecked() and v.type is 1) or (not self.checkPrio[v.priority].isChecked()) or (not self.matchesCriteria(v, search)):
                     continue
                     
                 self.addNewRow(v)
