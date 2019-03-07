d48f53230304d147113921e679e8ad43fdb27473
Segath <35260844+Segath@users.noreply.github.com>
2019-03-07 14:27:30 +0100

    - Quick update: Gamepad timeout ticks resets when there has been activity

--- src/package/controller/gamepad.py
+++ src/package/controller/gamepad.py
@@ -261,7 +261,10 @@ class Gamepad(QThread):
                     print(message)
                     UDP.ROVERSERVER.writeToRover(json.dumps(message, separators=(',', ':')))
             
-            ticks += 1
+            # Reset ticks when there has been activity from the controller
+            if eventHappened: ticks = 0
+            else: ticks += 1
+            
             if ticks >= GAMEPAD_TIMEOUT_TICK_TIME: # Check gamepad status regularly, DISCLAIMER: might break some button states?
                 self.refresh()
                 ticks = 0
